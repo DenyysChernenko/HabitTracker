078f8ee2e2636ab3e0279bbd5da2194b7b3ad94f
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-07-09 20:41:21 +0200

    Add first form, add base.html, improved habit list and user list

--- habit_tracker/habits/views.py
+++ habit_tracker/habits/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Habit
+from .forms import HabitForm
 
 # Create your views here.
 
@@ -7,3 +8,11 @@ from .models import Habit
 def habit_list(request):
     habits = Habit.objects.all()
     return render(request, 'habits/habit_list.html', {'habits': habits})
+
+
+def add_habit(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = HabitForm()
+        return render(request, 'habits/add_habit.html', {'form': form})
