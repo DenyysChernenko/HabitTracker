fdad8494bcc1611720dc9d11b767033bb1df9776
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-07-12 17:53:33 +0200

    Added Detail View

--- habit_tracker/habits/forms.py
+++ habit_tracker/habits/forms.py
@@ -1,13 +1,25 @@
 from django import forms
-from users.models import User
+from .models import Habit
+from django.forms import ModelForm
 
 
-class HabitForm(forms.Form):
-    name = forms.CharField()
-    description = forms.CharField()
-    start_date = forms.DateTimeField()
-    end_date = forms.DateTimeField()
-    current_streak = forms.IntegerField()
-    is_completed = forms.BooleanField()
+class HabitForm(ModelForm):
+    class Meta:
+        start_date = forms.DateTimeField(
+            widget=forms.DateTimeInput(attrs={'class': 'form-control', 'readonly': 'readonly'}))
 
-    user = forms.ModelChoiceField(queryset=User.objects.all())
+        model = Habit
+        fields = ['name', 'description', 'end_date', 'current_streak', 'is_completed', 'user']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'start_date': forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'end_date': forms.DateTimeInput(attrs={
+                'class': 'form-control',
+                'type': 'datetime-local'
+            }),
+            'current_streak': forms.NumberInput(attrs={'class': 'form-control'}),
+            'is_completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'user': forms.Select(attrs={'class': 'form-control'}),
+        }
+
--- habit_tracker/habits/views.py
+++ habit_tracker/habits/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Habit
 from .forms import HabitForm
 
 # Create your views here.
+
+
+def habit_detail(request, habit_id):
+    habit = get_object_or_404(Habit, pk=habit_id)
+    context = {
+        'habit': habit
+    }
+    return render(request, 'habits/habit_detail.html', context)
 
 
 def habit_list(request):
@@ -12,7 +20,10 @@ def habit_list(request):
 
 def add_habit(request):
     if request.method == 'POST':
-        pass
+        form = HabitForm(request.POST, request.FILES)
+        if form.is_valid():
+            habit = form.save()
+            return redirect('habit_detail', habit_id=habit.pk)
     else:
         form = HabitForm()
-        return render(request, 'habits/add_habit.html', {'form': form})
+    return render(request, 'habits/add_habit.html', {'form': form})
