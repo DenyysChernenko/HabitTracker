f77541ea92185e07e22e751d3b105341764e87d8
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-07-15 01:05:46 +0200

    Added UpdateView and DeleteView to User and Habit apps

--- habit_tracker/habits/urls.py
+++ habit_tracker/habits/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HabitListView, CreateHabit, HabitDetail
+from .views import HabitListView, CreateHabit, HabitDetail, UpdateHabit, DeleteHabit
 urlpatterns = [
     path('', HabitListView.as_view(), name='habit_list'),
     path('habits/add-habit', CreateHabit.as_view(), name='add_habit'),
     path('<int:pk>/', HabitDetail.as_view(), name='habit_detail'),
-
+    path('<int:pk>/edit/', UpdateHabit.as_view(), name='habit_update'),
+    path('<int:pk>/delete/', DeleteHabit.as_view(), name='habit_delete')
 ]
--- habit_tracker/habits/views.py
+++ habit_tracker/habits/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Habit
 from .forms import HabitForm
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -20,3 +21,19 @@ class HabitListView(ListView):
 class CreateHabit(CreateView):
     form_class = HabitForm
     template_name = 'habits/add_habit.html'
+
+
+class UpdateHabit(UpdateView):
+    model = Habit
+    fields = ['name', 'description', 'end_date', 'current_streak', 'is_completed']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('habit_list')
+
+    def get_success_url(self):
+        return reverse_lazy('habit_detail', kwargs={'pk': self.object.pk})
+
+
+class DeleteHabit(DeleteView):
+    model = Habit
+    success_url = reverse_lazy("habit_list")
+    template_name = "habits/delete_habit.html"
